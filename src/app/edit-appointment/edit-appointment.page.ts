b3a9d3c73c1472b20d30e52d8e503cddf89f3336
juanestebanalarcon <jalarcon_logyca@outlook.com>
2022-11-14 17:00:02 -0500

    Finalizado

--- src/app/edit-appointment/edit-appointment.page.ts
+++ src/app/edit-appointment/edit-appointment.page.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { AppointmentService } from './../shared/appointment.service';
-
 @Component({
   selector: 'app-edit-appointment',
   templateUrl: './edit-appointment.page.html',
@@ -11,18 +10,27 @@ import { AppointmentService } from './../shared/appointment.service';
 export class EditAppointmentPage implements OnInit {
   updateBookingForm: FormGroup;
   id: any;
-  constructor(private aptService: AppointmentService,private actRoute: ActivatedRoute,private router: Router,
+  constructor(
+    private aptService: AppointmentService,
+    private actRoute: ActivatedRoute,
+    private router: Router,
     public fb: FormBuilder
   ) {
     this.id = this.actRoute.snapshot.paramMap.get('id');
-    this.aptService.getBooking(this.id).valueChanges().subscribe(res => {this.updateBookingForm.setValue(res);});
+    this.aptService.getBooking(this.id).valueChanges().subscribe(res => {
+      this.updateBookingForm.setValue(res);
+    });
   }
   ngOnInit() {
-    this.updateBookingForm = this.fb.group({name: [''],email: [''],mobile: [''] });
+    this.updateBookingForm = this.fb.group({
+      name: [''],
+      email: [''],
+      mobile: ['']
+    });
     console.log(this.updateBookingForm.value);
   }
   updateForm() {
     this.aptService.updateBooking(this.id, this.updateBookingForm.value).then(() => {this.router.navigate(['/home']);})
-    .catch(error => console.log(error));
+      .catch(error => console.log(error));
   }
 }
